0246b7caf5b9a0211c1b65996b80bc6457950471
Gustavo Camalionti <gustavocamalionti@gmail.com>
2022-07-20 16:43:37 -0300

    Javascript Desafio - atualizar o desafio implementando funcionalidades

--- becas-santander-tecnologia-2022/2-modulo-html-css-js/introducao-js/estrutura-de-projeto/assets/js/scripts.js
+++ becas-santander-tecnologia-2022/2-modulo-html-css-js/introducao-js/estrutura-de-projeto/assets/js/scripts.js
@@ -1,15 +1,31 @@
-var currentNumberWrapper = document.getElementById('counter__currentNumber')
+var currentNumberWrapper = document.getElementById('counter__currentNumber');
 var currentNumber = 0;
+var buttonSomar = document.getElementById('somar');
+var buttonSubtrair = document.getElementById('subtrair');
 
+buttonSomar.addEventListener('click',increment);
+buttonSubtrair.addEventListener('click',decrement);
 
 
 function increment() {
     currentNumber = currentNumber + 1
     currentNumberWrapper.innerHTML = currentNumber;
-
+    verificationCurrentNumber()
 }
 
 function decrement() {
     currentNumber = currentNumber - 1
     currentNumberWrapper.innerHTML = currentNumber;
+    verificationCurrentNumber()
+}
+
+function verificationCurrentNumber() {
+    if (currentNumber == 0) {
+        buttonSubtrair.style.display = 'none';
+    } else if (currentNumber == 10) {
+        buttonSomar.style.display = 'none';
+    } else {
+        buttonSubtrair.style.display = 'inline';
+        buttonSomar.style.display = 'inline';
+    }
 }
